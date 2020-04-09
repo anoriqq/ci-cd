43878d9d106d179ec35373866d7d247d10bafcf8
anoriqq <shota.yoshikawa@anoriqq.com>
2020-04-09 21:12:45 +0900

    feat: update message

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,6 @@ import http from 'http';
 http
   .createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
+    res.end('Updated!');
   })
   .listen(8080, () => console.log('Server http://localhost:8080'));
