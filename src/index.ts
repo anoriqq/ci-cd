7dba8a5bb41f7d56f47485da97ea7a2025965762
anoriqq <shota.yoshikawa@anoriqq.com>
2020-04-11 21:37:48 +0900

    chore: update message

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,6 @@ import http from 'http';
 http
   .createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Updated at 2020-04-11 20:18');
+    res.end('Updated at 2020-04-11 21:37');
   })
   .listen(8080, () => console.log('Server http://localhost:8080'));
